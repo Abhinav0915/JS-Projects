de17af92555cfa77a8e2ea18fd0a100a29c213e3
Abhinav Saxena <abhinav150601@gmail.com>
2023-08-21 21:17:10 +0530

    started project

--- Toodle/app.js
+++ Toodle/app.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const path = require('path')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+const methodOverride = require ('method-override');
+const Schema = mongoose.Schema;
+
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
 
 //Listening at Port
 app.listen(3000, () => {
@@ -15,13 +20,23 @@ async function main() {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
-    console.log('MONGO CONNECTION IS SUCCESSFUL');
+    console.log('Database is now connected');
   } catch (error) {
     console.error('Connection Error:', error);
   }
 }
 mongoose.set('strictQuery', false);
 main().catch(err => console.log(err));
+
+//Model
+const taskSchema = new Schema({
+  task: String,
+  dueDays: String,
+  time: String,
+  status: String
+})
+
+const tasksDB = mongoose.model('allTasks', taskSchema);
 
 //Setting up view engine
 app.set('views',path.join(__dirname,'views'));  
@@ -33,8 +48,8 @@ app.use(express.static(path.join(__dirname,'public')))
 //Adding Routes
 
 //Homepage Route
-app.get('/', (req,res) => {
-    res.render('homepage')
+app.get('/home', (req,res) => {
+    res.render('homepage',{tasksDB})
 });
 
 //Add Task
@@ -42,4 +57,19 @@ app.get('/addTask', (req,res) => {
     res.render('addTask')
 });
 
+//Add Task
+app.post('/home', async (req,res) => {
+    const newTask = new tasksDB(req.body)
+    await newTask.save();
+    res.redirect('/home');
+    res.send('DONE!!!')
+});
 
+const item1 = new tasksDB({
+    task: 'Do Homework',
+    dueDays: 'Today',
+    time: '12:00',
+    status: 'Pending'
+});
+
+item1.save();
