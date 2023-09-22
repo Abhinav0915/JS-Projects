a82562a2808975f72cd31765097895f78c11ef88
Abhinav Saxena <abhinav150601@gmail.com>
2023-09-22 12:39:43 +0530

    tasks adding

--- Toodle/index.js
+++ Toodle/index.js
@@ -1,6 +1,30 @@
 const express = require("express");
-const app = express()
-const path = require('path')
+const path = require('path');
+const mongoose = require('mongoose');
+const app = express();
+
+// Set the strictQuery option before connecting
+mongoose.set('strictQuery', false);
+
+main().catch(err => console.log(err));
+
+async function main() {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/toodle');
+    console.log('Connection Successful');
+  } catch (error) {
+    console.error('Connection Error:', error);
+  }
+}
+
+const toodleSchema = new mongoose.Schema({
+    task: String
+});
+
+const Toodle = mongoose.model('Toodle', toodleSchema);
+
+app.use(express.urlencoded({ extended: true }));
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,'/views'))
@@ -10,6 +34,19 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
 
-app.get('/', (req,res) => {
-    res.render('home')
+app.get('/', async (req,res) => {
+    const toodle = await Toodle.find({})
+    res.render('home', {toodle})
+})
+
+app.get('/addTask', (req,res) =>{
+    res.render('addTask')
+})
+
+app.post('/addNewTask',(req,res)=>{
+    const newTask = new Toodle({
+        task: req.body.task
+    })
+    newTask.save()
+    res.redirect('/') 
 })
